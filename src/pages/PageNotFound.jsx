0d567cf0d81adf2e39bb11693b36dfed67b5e4f7
akshayv1811 <akshayv1811@gmail.com>
2024-12-12 12:28:49 +0530

    Built the pages

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import PageNav from '../components/PageNav';
-
 export default function PageNotFound() {
   return (
-    <div><PageNav/>Not found</div>
-  )
+    <div>
+      <h1>Page not found 😢</h1>
+    </div>
+  );
 }
