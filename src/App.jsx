3b37fc120151fb2b7b5b3b8a33c8722722b0c42b
akshayv1811 <akshayv1811@gmail.com>
2025-02-04 16:15:54 +0530

    Programmatic Navigation with useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -10,6 +10,7 @@ import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form"
 
 const BASE_URL = "http://localhost:9000";
 
@@ -52,7 +53,7 @@ export default function App() {
           <Route path = 'cities/:id' element = {<City/>} />
           <Route path="countries" element={<CountryList cities = {cities} 
             isLoading = {isLoading} />} />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form/>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
