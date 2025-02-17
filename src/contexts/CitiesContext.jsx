a21b58f9d7adbd72d373f1b8a127783722aecb8a
akshayv1811 <akshayv1811@gmail.com>
2025-02-17 12:10:48 +0530

    Consuming the CitiesContext

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -36,4 +36,10 @@ function CitiesProvider({ children }) {
   );
 }
 
-export {CitiesProvider}
+function useCities(){
+    const context = useContext(CitiesContext);
+    if (context === undefined) throw new Error('CitiesContext was used outsde the CitiesProvider');
+    return context;
+}
+
+export {CitiesProvider, useCities};
