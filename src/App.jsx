51b951d7322fa570e2345fc6bb95f99eec3db30e
akshayv1811 <akshayv1811@gmail.com>
2025-02-17 11:24:44 +0530

    Created a CitiesContext

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,18 @@ import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import CityList from "./components/CityList";
-import { useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
+import { CitiesProvider } from "./contexts/CitiesContext";
 
-const BASE_URL = "http://localhost:9000";
+
 
 export default function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch {
-        alert("There was an error loading data...");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
+ 
 
   return (
+    <CitiesProvider>
     <BrowserRouter>
       <Routes>
         <Route index element={<Homepage />} />
@@ -45,17 +29,18 @@ export default function App() {
           <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
-            element={<CityList cities={cities} isLoading={isLoading} />}
+            element={<CityList />}
           />
           <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
-            element={<CountryList cities={cities} isLoading={isLoading} />}
+            element={<CountryList/>}
           />
           <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
+    </CitiesProvider>
   );
 }
