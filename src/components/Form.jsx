3b0aafa9fd43186f7e33148d3c904eeef20211d5
akshayv1811 <akshayv1811@gmail.com>
2025-02-17 22:58:01 +0530

    Finishing the City View

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./Form.module.css";
 import Button from "./Button";
+import BackButton from "./BackButton";
 import "./Button.module.css";
 
 export function convertToEmoji(countryCode) {
@@ -15,7 +16,7 @@ export function convertToEmoji(countryCode) {
 }
 
 function Form() {
-  const navigate = useNavigate();
+ 
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
@@ -53,14 +54,8 @@ function Form() {
 
       <div className={styles.buttons}>
         <Button type="primary">Add</Button>
-        <Button type="back"
-          onClick={(e) => {
-            e.preventDefault();
-            navigate(-1);
-          }}
-        >
-          &larr; Back
-        </Button>
+        <BackButton/>
+        
       </div>
     </form>
   );
